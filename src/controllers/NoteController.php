2feac3b9d600add15bab82826ac0354401ccc460
Jensone <hello@jensone.com>
2025-04-24 11:21:12 +0200

    Ajout de la méthode add dans NoteController pour créer et enregistrer une nouvelle note à partir de données POST

--- src/controllers/NoteController.php
+++ src/controllers/NoteController.php
@@ -8,6 +8,22 @@ use models\Note;
 
 class NoteController extends AbstractController
 {
+    static public function add()
+    {
+        $nouvelleNote = new Note(); // Instanciation de la classe Note avec un objet $nouvelleNote
+        $nouvelleNote
+            ->setTitle($_POST['title']) // Récupération du contenu de la variable $_POST['title']
+            ->setSlug(uniqid('note_')) // Création d'un nouveau slug
+            ->setContent($_POST['content']) // Récupération du contenu de la variable $_POST['content']
+        ;
+
+        $nouvelleNote->bindValues(); // Liaison des variables avec les propriétés de la classe Note
+        $nouvelleNote->create(); // Enregistrement de la nouvelle note dans la base de données
+
+        var_dump($nouvelleNote); // Affiche les informations de la nouvelle note
+        die();
+    }
+
     static public function edit($slug)
     {
         // Get actual data of the note
